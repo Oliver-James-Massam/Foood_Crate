d964697f83c24b72f9f557da88efd2463cf88ea1
BaronVonBaconHam <michael.mommsen@gmail.com>
2016-10-17 19:49:51 +0200

    Test connection to local mysql database

--- DatabaseService/DatabaseService/DBService.svc.cs
+++ DatabaseService/DatabaseService/DBService.svc.cs
@@ -5,12 +5,13 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace DatabaseService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class Service1 : DBService
     {
         public string GetData(int value)
         {
@@ -28,6 +29,17 @@ namespace DatabaseService
                 composite.StringValue += "Suffix";
             }
             return composite;
+        }
+
+        public string TestConnection()
+        {
+
+            string connectionString = @"server=localhost;user id=Test;Password=test;persistsecurityinfo=True;database=db_foodcrate";
+            using (MySqlConnection cn = new MySqlConnection(connectionString))
+            {
+                cn.Open();
+                return "Connection open";
+            }
         }
     }
 }
