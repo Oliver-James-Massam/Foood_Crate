2b7ab025b866438b4a00909079885eb2149a2301
Lance Govender <lg452323@gmail.com>
2016-10-26 10:57:14 +0200

    Added admin page

---
+++ FoodCrate_V1.01/FoodCrate_V1.01/Pages/AdminPage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FoodCrate_V1._01.MasterPage
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*
+            if (Session["Admin"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Redirect("Pages/login.aspx");
+            }*/
+        }
+    }
+}
