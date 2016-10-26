e90165568e8a197bbcbc3c3e22ec64d3c577adeb
BaronVonBaconHam <michael.mommsen@gmail.com>
2016-10-26 17:12:34 +0200

    Commiting old db service stuff to master because lance said he'd put everything in one but didn't

--- DatabaseService/DatabaseService/DBService.svc.cs
+++ DatabaseService/DatabaseService/DBService.svc.cs
@@ -5,13 +5,16 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace DatabaseService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class Service1 : DBService
     {
+
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
@@ -28,6 +31,17 @@ namespace DatabaseService
                 composite.StringValue += "Suffix";
             }
             return composite;
+        }
+
+        public string TestConnection()
+        {
+
+            string connectionString = @"server=localhost;user id=admin;Password=Foodcrate1;persistsecurityinfo=True;database=foodcratedb";
+            using (MySqlConnection cn = new MySqlConnection(connectionString))
+            {
+                cn.Open();
+                return "Connection open";
+            }
         }
     }
 }
