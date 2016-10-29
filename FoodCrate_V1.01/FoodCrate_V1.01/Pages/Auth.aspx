c4b2f9efc963d7cb9e63b45841dad78b65f7f144
Lance Govender <lg452323@gmail.com>
2016-10-29 19:39:02 +0200

    Readded auth, fixed edit prodcut page

---
+++ FoodCrate_V1.01/FoodCrate_V1.01/Pages/Auth.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/Global.Master" AutoEventWireup="true" CodeBehind="Auth.aspx.cs" Inherits="FoodCrate_V1._01.MasterPage.WebForm12" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+    <asp:ListView ID="ListView1" runat="server">
+        <ItemTemplate>
+            <table>
+                <tr>
+                    <td>link:</td>
+                    <td><%# Eval("link") %><br />
+                    </td>
+                </tr>
+                <tr>
+                    <td>First Name:</td>
+                    <td><%# Eval("first_name") %><br />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Last Name:</td>
+                    <td><%# Eval("last_name") %><br />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><%# Eval("email") %><br />
+                    </td>
+                </tr>
+                <tr>
+                </tr>
+            </table>
+        </ItemTemplate>
+    </asp:ListView>
+</div>
+</asp:Content>
---
+++ FoodCrate_V1.01/FoodCrate_V1.01/Pages/Auth.aspx.cs
@@ -0,0 +1,63 @@
+﻿using FoodCrate_V1._01.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FoodCrate_V1._01.MasterPage
+{
+    public partial class WebForm12 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            checkFBAuth();
+        }
+
+        private void checkFBAuth()
+        {
+
+            String id = ConfigurationManager.AppSettings["FacebookID"];
+            String app_secret = ConfigurationManager.AppSettings["FaceSecret"];
+
+            if (Request.QueryString["code"] != "")
+            {
+
+                Uri destuUi = new Uri("https://graph.facebook.com/oauth/access_token?client_id=" + id + "&client_secret=" + app_secret + "&redirect_uri=http://" + Request.ServerVariables["SERVER_NAME"] + ":" + Request.ServerVariables["SERVER_PORT"] + "/Pages/Auth.aspx&code=" + Request.QueryString["code"]);
+                HttpWebRequest httpReq = (HttpWebRequest)HttpWebRequest.Create(destuUi);
+                System.IO.StreamReader reader = new System.IO.StreamReader(httpReq.GetResponse().GetResponseStream());
+                string token = reader.ReadToEnd().ToString().Replace("access_token=", "");
+                string[] combined = token.Split('&');
+                string accessToken = combined[0];
+                Uri eatTargetUri = new Uri("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=" + id + "&client_secret=" + app_secret + "&fb_exchange_token=" + accessToken); HttpWebRequest eat = (HttpWebRequest)HttpWebRequest.Create(eatTargetUri);
+
+                StreamReader eatStr = new StreamReader(eat.GetResponse().GetResponseStream());
+                string eatToken = eatStr.ReadToEnd().ToString().Replace("access_token=", "");
+
+                string[] eatWords = eatToken.Split('&');
+                string extendedAccessToken = eatWords[0];
+
+                Uri targetUserUri = new Uri("https://graph.facebook.com/me?fields=first_name,last_name,link,email&access_token=" + accessToken);
+                HttpWebRequest user = (HttpWebRequest)HttpWebRequest.Create(targetUserUri);
+
+                StreamReader userInfo = new StreamReader(user.GetResponse().GetResponseStream());
+                string jsonResponse = string.Empty;
+                jsonResponse = userInfo.ReadToEnd();
+
+                JavaScriptSerializer sr = new JavaScriptSerializer();
+                string jsondata = jsonResponse;
+                FackebookStrut.User converted = sr.Deserialize<FackebookStrut.User>(jsondata);
+
+                List<FackebookStrut.User> currentUser = new List<FackebookStrut.User>();
+                currentUser.Add(converted);
+                ListView1.DataSource = currentUser;
+                ListView1.DataBind();
+            }
+        }
+    }
+}
