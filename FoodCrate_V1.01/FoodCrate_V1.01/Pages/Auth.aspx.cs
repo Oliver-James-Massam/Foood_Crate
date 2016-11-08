c2d3e0034907fc5ce87752506f7ec5580135afd2
Lance Govender <lg452323@gmail.com>
2016-11-08 23:05:45 +0200

    get user name

--- FoodCrate_V1.01/FoodCrate_V1.01/Pages/Auth.aspx.cs
+++ FoodCrate_V1.01/FoodCrate_V1.01/Pages/Auth.aspx.cs
@@ -73,6 +73,7 @@ namespace FoodCrate_V1._01.MasterPage
             {
                 // login current user
                 int iRank = data.AuthUser(fb[0].email, fb[0].link);
+                Session["user"] = fb[0].first_name + " " + fb[0].last_name;
                 // rank user
                 switch (iRank)
                 {
@@ -103,6 +104,7 @@ namespace FoodCrate_V1._01.MasterPage
                 // create a new user
                 data.AddUser(fb[0].email, fb[0].first_name, fb[0].last_name, fb[0].email, 1, fb[0].link);
                 int iRank = data.AuthUser(fb[0].email, fb[0].link);
+                Session["user"] = fb[0].first_name + " " + fb[0].last_name;
                 // rank user
                 switch (iRank)
                 {
