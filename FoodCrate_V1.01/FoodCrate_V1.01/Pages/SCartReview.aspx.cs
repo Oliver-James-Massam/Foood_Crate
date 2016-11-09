4b93296c8a7b231cfcd3f38ce5394746a7eb4b09
Lance Govender <lg452323@gmail.com>
2016-11-09 23:13:45 +0200

    cart logic

--- FoodCrate_V1.01/FoodCrate_V1.01/Pages/SCartReview.aspx.cs
+++ FoodCrate_V1.01/FoodCrate_V1.01/Pages/SCartReview.aspx.cs
@@ -9,13 +9,27 @@ namespace FoodCrate_V1._01.MasterPage
 {
     public partial class WebForm9 : System.Web.UI.Page
     {
+        double value = 10;
         protected void Page_Load(object sender, EventArgs e)
         {
-     
-    }
+            // value = double.Parse(Request.QueryString["Total"]);
+            FaceVal.Text = value.ToString();
+            Tax.Text = (value * 0.14).ToString();
+            if (value < 300)
+            {
+                Shipping.Text = "Not free - R" + value*0.05;
+                Total.Text = (value * 0.14 + value * 0.05 + value).ToString();
+            }
+            else
+            {
+                Shipping.Text = "Free";
+                Total.Text = (value * 0.14 + value * 0.05 + value).ToString();
+            }
+
+         }
         protected void Accept_Click(object sender, EventArgs e)
         {
-
+            // add invoice
 
             Response.Redirect("../Pages/ScartCheckOut.aspx");
         }
